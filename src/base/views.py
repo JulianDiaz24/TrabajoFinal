209124dd098e6d62cd140d3f20616f3cee33e1a7
Julian Diaz <juliixd5@gmial.com>
2025-06-30 20:47:32 -0300

    cosas

--- src/base/views.py
+++ src/base/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
 # Create your views here.
+
+def saludar(request):
+    return HttpResponse(" Te amo !")
+
+def bicha(request):
+    return HttpResponse("pene")
